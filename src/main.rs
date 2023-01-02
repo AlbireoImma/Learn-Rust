fb754ae22dec65f8792a3879dea412e108275791
Francisco Abarca <faam1612@gmail.com>
2023-01-02 16:00:49 -0300

    Testcase: List

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,16 @@
 //mod formatted_print;
 //mod debug_print;
-mod display;
+//mod display;
+mod testcase_list;
 
 fn main() {
+    //Chapter 1: Hello World
+    
     //println!("Hello, world!");
     //println!("I'm a Rustacean!");
 
     //formatted_print::formatted();
     //debug_print::debug_print();
-    display::display_rust();
+    //display::display_rust();
+    testcase_list::testcase();
 }
